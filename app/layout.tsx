d922f1421000a52c5df958b583291c9516785d10
Jahongir Hayitov <Jahongirhacking@gmail.com>
2024-06-25 19:02:58 +0500

    init: create a new project

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "@styles/globals.scss";
+
+export const metadata: Metadata = {
+  title: "Sertifikat.edu.uz",
+  description: "Tan olingan xorijiy tillar hamda umumta’lim fаnlаri bo‘yicha xalqaro sertifikatlar bazasi",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="uz">
+      <body>{children}</body>
+    </html>
+  );
+}
