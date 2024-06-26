b6b6923cdae1138321911a01cb358da0ba0716b6
Jahongirhacking <Jahongirhacking@gmail.com>
2024-06-26 17:09:28 +0500

    upd: change folder name

---
+++ app/_assets/images/main/certificate_icons/index.ts
@@ -0,0 +1,102 @@
+import ICertificateIcon from "@/app/_types/main_page/ICertificateIcon";
+
+export { default as usa } from "./1-USA-flag.png";
+export { default as telescope } from "./2-telescope.png";
+export { default as germany } from "./3-Germany-flag.png";
+export { default as gene } from "./4-gene.png";
+export { default as france } from "./5-France-flag.png";
+export { default as chemistry } from "./6-chemistry.png";
+export { default as italy } from "./7-Italy-flag.png";
+export { default as laptop } from "./8-laptop.png";
+export { default as spain } from "./9-Spain-flag.png";
+export { default as abacus } from "./10-abacus.png";
+export { default as turkey } from "./11-Turkey-flag.png";
+export { default as lightBulb } from "./12-light-bulb.png";
+export { default as uae } from "./13-UAE-flag.png";
+export { default as magnifyingGlass } from "./14-magnifying-glass.png";
+export { default as southKorea } from "./15-South-Korea.png";
+export { default as pen } from "./16-pen.png";
+export { default as japan } from "./17-Japan-flag.png";
+export { default as book } from "./18-book.png";
+export { default as china } from "./19-China-flag.png";
+
+const listOfHeroIcons: ICertificateIcon[] = [
+  {
+    key: "usa",
+    title: "Ingliz tili",
+  },
+  {
+    key: "telescope",
+    title: "Astronomiya",
+  },
+  {
+    key: "germany",
+    title: "Nemis tili",
+  },
+  {
+    key: "gene",
+    title: "Biologiya",
+  },
+  {
+    key: "france",
+    title: "Fransuz tili",
+  },
+  {
+    key: "chemistry",
+    title: "Kimyo",
+  },
+  {
+    key: "italy",
+    title: "Italian tili",
+  },
+  {
+    key: "laptop",
+    title: "Informatika",
+  },
+  {
+    key: "spain",
+    title: "Ispan tili",
+  },
+  {
+    key: "abacus",
+    title: "Matematika",
+  },
+  {
+    key: "turkey",
+    title: "Turk tili",
+  },
+  {
+    key: "lightBulb",
+    title: "Fizika",
+  },
+  {
+    key: "uae",
+    title: "Arab tili",
+  },
+  {
+    key: "magnifyingGlass",
+    title: "Geografiya",
+  },
+  {
+    key: "southKorea",
+    title: "Koreys tili",
+  },
+  {
+    key: "pen",
+    title: "Ona tili va adabiyot",
+  },
+  {
+    key: "japan",
+    title: "Yapon tili",
+  },
+  {
+    key: "book",
+    title: "Huquqshunoslik",
+  },
+  {
+    key: "china",
+    title: "Xitoy tili",
+  },
+];
+
+export default listOfHeroIcons;
