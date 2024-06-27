bf057781b3c9ce67e9cb622db0407e90102f052b
Jahongirhacking <Jahongirhacking@gmail.com>
2024-06-27 23:00:44 +0500

    init: identify image extensions

---
+++ declarations.d.ts
@@ -0,0 +1,24 @@
+declare module "*.jpg" {
+  const value: StaticImageData;
+  export default value;
+}
+
+declare module "*.png" {
+  const value: StaticImageData;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: StaticImageData;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: StaticImageData;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: StaticImageData;
+  export default value;
+}
