b66f0068e0b68bd0803a4648a5b0761ac74ee76f
Jahongirhacking <Jahongirhacking@gmail.com>
2024-06-27 23:04:15 +0500

    init: upload card icon images

---
+++ app/_assets/images/main/card_icons/index.ts
@@ -0,0 +1,5 @@
+export { default as image1 } from "./teacher.png";
+export { default as image2 } from "./connect.png";
+export { default as image3 } from "./company.png";
+export { default as image4 } from "./document.png";
+export { default as image5 } from "./info.png";
