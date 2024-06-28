cc539ca9859342130217ab3e9667cd23b43b4f67
Jahongirhacking <Jahongirhacking@gmail.com>
2024-06-28 15:21:51 +0500

    feat: import social network logos

---
+++ app/_assets/images/main/social_networks/index.ts
@@ -0,0 +1,3 @@
+export { default as youtube } from "@/app/_assets/images/main/social_networks/Youtube.png";
+export { default as instagram } from "@/app/_assets/images/main/social_networks/Instagram.png";
+export { default as facebook } from "@/app/_assets/images/main/social_networks/Facebook.png";
