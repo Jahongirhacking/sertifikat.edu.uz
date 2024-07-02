0615c58253015d1040bacd976e39b4d3fd1cbfc0
Jahongirhacking <Jahongirhacking@gmail.com>
2024-07-02 09:10:12 +0500

    fix: make AuthButtons fake Modal

--- app/_components/main_page/AuthButtons.tsx
+++ app/_components/main_page/AuthButtons.tsx
@@ -1,15 +1,113 @@
-import { Button } from "antd"
-import Link from "next/link"
+"use client";
+
+import { Button, Form, Input, Modal, Space, Tabs, TabsProps } from "antd";
+import "./AuthButtons.scss";
+import { useState } from "react";
+import SignInForm from "../forms/SignInForm";
+import SignUpForm from "../forms/SignUpForm";
+import Link from "next/link";
+import { LeftOutlined } from "@ant-design/icons";
+
+type SMSFieldType = {
+    code?: string;
+};
 
 const AuthButtons = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [showModalSMS, setShowModalSMS] = useState(false);
+    const [activeTab, setActiveTab] = useState<"signIn" | "signUp">("signIn")
+
+    const items: TabsProps['items'] = [
+        {
+            key: 'signIn',
+            label: 'Kirish',
+            children: <SignInForm />,
+        },
+        {
+            key: 'signUp',
+            label: 'Ro‘yxatdan o‘tish',
+            children: <SignUpForm
+                setShowModalSMS={setShowModalSMS}
+                setShowModal={setShowModal}
+            />,
+        },
+    ];
+
     return (
         <div className="auth-btns hero__auth-btns">
-            <Link href={"./cabinet"}>
-                <Button className="btn login-btn">Tizimga kirish</Button>
-            </Link>
-            <Link href={"./cabinet"}>
-                <Button className="btn signup-btn">Ro‘yxatdan o‘tish</Button>
-            </Link>
+            <Button
+                className="btn login-btn"
+                onClick={() => {
+                    setActiveTab("signIn");
+                    setShowModal(true)
+                }}
+            >Tizimga kirish</Button>
+            <Button
+                className="btn signup-btn"
+                onClick={() => {
+                    setActiveTab("signUp");
+                    setShowModal(true)
+                }}
+            >Ro‘yxatdan o‘tish</Button>
+
+            <Modal
+                open={showModal}
+                onCancel={() => setShowModal(false)} footer={null}
+            >
+                <Tabs
+                    activeKey={activeTab}
+                    items={items}
+                    onChange={(key: string) => setActiveTab(key as "signIn" | "signUp")}
+                />
+            </Modal>
+
+            <Modal
+                title={"SMS kodni tasdiqlash"}
+                open={showModalSMS}
+                onCancel={() => { setShowModalSMS(false) }}
+                footer={null}
+            >
+                <p style={{ paddingBottom: "15px" }}>
+                    SMS kodni <b>+998903718891</b> raqamiga yubordik. Telefon raqamingizni tasdiqlash uchun quyidagi maydonga kodni kiriting.
+                </p>
+
+                <Form
+                    name="sms-code"
+                    initialValues={{ remember: true }}
+                    autoComplete="off"
+                >
+                    <Form.Item<SMSFieldType>
+                        label="SMS kod:"
+                        name="code"
+                        rules={[{ required: true, message: 'Iltimos SMS kod kiriting!' }]}
+                    >
+                        <Input placeholder="SMS kodni kiriting" />
+                    </Form.Item>
+
+                    <Link href="#" style={{ display: "inline-block", paddingBottom: "15px" }}>
+                        SMS kodni qayta yuborish
+                    </Link>
+
+                    <Space direction="vertical" >
+                        <Link href="/cabinet">
+                            <Button
+                                onClick={() => {
+                                    setShowModalSMS(true);
+                                    setShowModal(false);
+                                }}
+                                type="primary"
+                                htmlType="submit"
+                            >
+                                Davom ettirish
+                            </Button>
+                        </Link>
+                        <Button disabled>
+                            <LeftOutlined />
+                            <span>Orqaga qaytish</span>
+                        </Button>
+                    </Space>
+                </Form>
+            </Modal>
         </div>
     )
 }
--- app/_components/main_page/CertificateTable.tsx
+++ app/_components/main_page/CertificateTable.tsx
@@ -1,16 +1,37 @@
 import ICertificateTable, { IColumn, ILanguageTable, IScienceTable } from "@/app/_types/main_page/ICertificateTable"
-import { Flex, Radio, Table, Tabs } from "antd"
-import type { TabsProps } from 'antd';
+import { Button, Dropdown, Flex, Radio, Space, Table, Tabs } from "antd"
+import type { MenuProps, TabsProps } from 'antd';
 import Image from "next/image"
 import * as CardIcons from "@/app/_assets/images/main/card_icons/index"
+import { DownOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 const CertificateTable = <T extends object>({ professions, specialties, info, options, data, columns, dataType }: ICertificateTable<T>) => {
+    const [professionActive, setProfessionActive] = useState(0);
+    const [specialityActive, setSpecialityActive] = useState(0);
+
     const CustomTable = <Table className={dataType} dataSource={data} columns={columns} />
     const items: TabsProps['items'] = options.map((option, index) => ({
         key: `${index}`,
         label: option.label,
         children: CustomTable
     }))
+
+    const professionMenuProps: MenuProps = {
+        items: professions.map((profession, index) => ({
+            key: index,
+            label: profession.label
+        })),
+        onClick: (e: any) => setProfessionActive(e.key)
+    }
+
+    const specialtyMenuProps: MenuProps = {
+        items: specialties.map((profession, index) => ({
+            key: index,
+            label: profession.label
+        })),
+        onClick: (e: any) => setSpecialityActive(e.key)
+    }
 
     return (
         <div className="table-container">
@@ -26,6 +47,14 @@ const CertificateTable = <T extends object>({ professions, specialties, info, op
                             optionType="button"
                             buttonStyle="solid"
                         />
+                        <Dropdown menu={professionMenuProps}>
+                            <Button>
+                                <Space>
+                                    {professions[professionActive].label}
+                                    <DownOutlined />
+                                </Space>
+                            </Button>
+                        </Dropdown>
                     </label>
                     <label className="radio-label">
                         <span className="label-text">
@@ -37,6 +66,15 @@ const CertificateTable = <T extends object>({ professions, specialties, info, op
                             optionType="button"
                             buttonStyle="solid"
                         />
+
+                        <Dropdown menu={specialtyMenuProps}>
+                            <Button>
+                                <Space>
+                                    {specialties[specialityActive].label}
+                                    <DownOutlined />
+                                </Space>
+                            </Button>
+                        </Dropdown>
                     </label>
                 </Flex>
                 <div className="info-container">
