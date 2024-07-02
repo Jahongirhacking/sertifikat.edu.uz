46fd9d8618978d6f2946307777ef8bdfcbf41f24
Jahongirhacking <Jahongirhacking@gmail.com>
2024-07-02 18:05:17 +0500

    feat: add Profile button link

--- app/_components/Info.tsx
+++ app/_components/Info.tsx
@@ -10,7 +10,7 @@ const Info = ({ items }: InfoProps) => {
     return (
         <>
             {items.map((item, index) => (
-                <Row key={index} className="certificates-row">
+                <Row key={index} className="info-row">
                     <Col span={10} className="left-text"><span>{item.left}</span></Col>
                     <Col span={14} className="right-text">{
                         item.right?.href
